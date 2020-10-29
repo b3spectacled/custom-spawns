493f553f4578412fd212f2d6416c65befa70f8e6
b3spectacled <not@email.com>
2020-10-29 00:31:06 -0700

    Tamed mobs no longer despawn, regardless of persistence setting.

---
+++ src/main/java/com/bespectacled/customspawn/mixin/MixinTameableEntity.java
@@ -0,0 +1,31 @@
+package com.bespectacled.customspawn.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import com.bespectacled.customspawn.CustomSpawn;
+
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.entity.passive.TameableEntity;
+
+@Mixin(TameableEntity.class)
+public class MixinTameableEntity extends MobEntity {
+    @Shadow
+    private static TrackedData<Byte> TAMEABLE_FLAGS;
+    
+    protected MixinTameableEntity() {
+        super(null, null);
+    }
+    
+    @Shadow
+    public boolean isTamed() {
+        return ((Byte)this.dataTracker.get(TAMEABLE_FLAGS) & 4) != 0;
+    }
+
+    @Override
+    public boolean canImmediatelyDespawn(double distance) {
+        return !(CustomSpawn.SPAWNS_CONFIG.passivePersistent || this.isTamed());
+    }
+    
+}
