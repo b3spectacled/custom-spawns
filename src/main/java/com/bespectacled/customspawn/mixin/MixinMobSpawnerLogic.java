920f4c5cb3e807c43005b8df1825e7884c6bd350
b3spectacled <modernbetamc@gmail.com>
2021-11-25 14:58:01 -0800

    Fix critical issue with mob spawner value initialization. Improve compatibility with Enhanced Mob Spawners mod.

--- src/main/java/com/bespectacled/customspawn/mixin/MixinMobSpawnerLogic.java
+++ src/main/java/com/bespectacled/customspawn/mixin/MixinMobSpawnerLogic.java
@@ -9,17 +9,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.bespectacled.customspawn.CustomSpawn;
 
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.MobSpawnerLogic;
+import net.minecraft.world.World;
 
 @Mixin(MobSpawnerLogic.class)
 public class MixinMobSpawnerLogic {
-    @Unique private static final int DEFAULT_MIN_SPAWN_DELAY = 200;
-    @Unique private static final int DEFAULT_MAX_SPAWN_DELAY = 800;
-    @Unique private static final int DEFAULT_SPAWN_COUNT = 4;
-    @Unique private static final int DEFAULT_MAX_NEARBY_ENTITIES = 6;
-    @Unique private static final int DEFAULT_REQUIRED_PLAYER_RANGE = 16;
-    @Unique private static final int DEFAULT_SPAWN_RANGE = 4;
-    
     @Shadow private int minSpawnDelay;
     @Shadow private int maxSpawnDelay;
     @Shadow private int spawnCount;
@@ -27,8 +23,8 @@ public class MixinMobSpawnerLogic {
     @Shadow private int requiredPlayerRange;
     @Shadow private int spawnRange;
     
-    @Inject(method = "serverTick", at = @At("HEAD")) 
-    private void injectUpdate(CallbackInfo info) {
+    @Inject(method = "readNbt", at = @At("TAIL")) 
+    private void injectReadNbt(World world, BlockPos pos, NbtCompound nbt, CallbackInfo info) {
         if (CustomSpawn.SPAWNS_CONFIG.overrideSpawnerDefaultValues) {
             this.minSpawnDelay = CustomSpawn.SPAWNS_CONFIG.minSpawnDelay;
             this.maxSpawnDelay = CustomSpawn.SPAWNS_CONFIG.maxSpawnDelay;
@@ -36,13 +32,6 @@ public class MixinMobSpawnerLogic {
             this.maxNearbyEntities = CustomSpawn.SPAWNS_CONFIG.maxNearbyEntities;
             this.requiredPlayerRange = CustomSpawn.SPAWNS_CONFIG.requiredPlayerRange;
             this.spawnRange = CustomSpawn.SPAWNS_CONFIG.spawnRange;
-        } else {
-            this.minSpawnDelay = DEFAULT_MIN_SPAWN_DELAY;
-            this.maxSpawnDelay = DEFAULT_MAX_SPAWN_DELAY;
-            this.spawnCount = DEFAULT_SPAWN_COUNT;
-            this.maxNearbyEntities = DEFAULT_MAX_NEARBY_ENTITIES;
-            this.requiredPlayerRange = DEFAULT_REQUIRED_PLAYER_RANGE;
-            this.spawnRange = DEFAULT_SPAWN_RANGE;
         }
     }
 }
